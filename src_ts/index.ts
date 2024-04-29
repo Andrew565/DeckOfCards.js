54bc3e225dc474c781eec68c8e13902257ed01a1
Andrew Steele <andrew.steele@adhocteam.us>
2024-04-29 08:46:21 -0400

    Deleting no-longer-needed old files

--- src_ts/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as cardRankArray } from "./cardRankArray";
-export { DeckOfCards, baseDeck } from "./DeckOfCards";
-export { default as StandardCards } from "./standard52CardsAndJokers";
